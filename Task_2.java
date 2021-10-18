9f9fd754eb594f3afd82ab6b671ad0263a453086
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-18 21:49:35 +0200

    Update Task_2.java

--- Task_2.java
+++ Task_2.java
@@ -5,7 +5,7 @@ package task_2;
 public class Task_2 {
     
     //function
-    public static double func(int a, int b, int c) {  
+    public static double func(double a, double b, double c) {  
         
         double pZ = ( b + Math.sqrt( Math.pow(b, 2) + 4 * a * c ) ) / 2 * a - Math.pow(a, 3) * c + Math.pow(b, -2);
         return pZ;  
