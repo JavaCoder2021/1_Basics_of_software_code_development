52149bf95c1a65d56a11d092fafc540faf6ec46f
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-18 21:24:07 +0200

    Update Task_6.java

--- Task_6.java
+++ Task_6.java
@@ -4,7 +4,7 @@ public class Task_6 {
     
     public static void trueOrFalse(int x, int y) {
         
-        if (x<-6 || x>8 || y<-4 || y>7 || x==0 || y ==0)
+        if (x<-6 || x>8 || y<-4 || y>7 || x==0 || y==0)
         {
             System.out.println("Неправильно задана область x и y");
         }
