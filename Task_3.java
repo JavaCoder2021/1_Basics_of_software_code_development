c78c5706a3e1f1d09180a2e58518b42d4b5f8bbd
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-18 19:01:41 +0200

    Add files via upload

---
+++ Task_3.java
@@ -0,0 +1,28 @@
+package task_3;
+
+public class Task_3 {
+    
+    //Функция
+    public static double func(double x, double y) {
+        
+        double radiansX = Math.toRadians(x);
+        double radiansY = Math.toRadians(y);
+        double radiansXY = Math.toRadians(x * y);
+        double cosX = Math.cos(radiansX);
+        double cosY = Math.cos(radiansY);
+        double sinX = Math.sin(radiansX);
+        double sinY = Math.sin(radiansY);
+        double tanXY = Math.tan(radiansXY);
+        double pZ = ((sinX + cosY) / (cosX - sinY)) * tanXY;
+        return pZ;
+       
+    }
+
+    public static void main(String[] args) {
+        
+        double z = func(60, 40);
+        System.out.printf("z = %.1f \n", z);
+       
+    }
+    
+}
