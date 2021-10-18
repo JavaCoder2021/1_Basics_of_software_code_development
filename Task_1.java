d9afdd06da608658ce952e335f53f0a17e99984c
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-18 20:24:12 +0200

    Update Task_1.java

--- Task_1.java
+++ Task_1.java
@@ -3,17 +3,17 @@ package task_1;
 public class Task_1 {
     
     //function
-    public static int func(int a, int b, int c) {  
+    public static double func(double a, double b, double c) {  
         
-        int pZ = (( a - 3 ) * b/2 ) + c; 
+        double pZ = (( a - 3 ) * b/2 ) + c; 
         return pZ;  
         
     }
 
     public static void main(String[] args) {
         
-        int z = func(1, 2, 3);
-        System.out.println("z = " + z);
+        double z = func(1, 2, 3);
+        System.out.printf("z = %.1f \n", z);
         
     }
     
