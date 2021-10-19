bcad5f3dbb4d53964e9e648458313270879748cb
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-19 19:44:30 +0200

    Add files via upload

---
+++ Task_24.java
@@ -0,0 +1,22 @@
+//Циклы. 4. Составить программу нахождения произведения квадратов первых двухсот чисел.
+
+package task_24;
+
+import java.math.*;
+
+public class Task_24 {
+         
+    public static void main(String[] args) {
+        
+        BigInteger proizv = BigInteger.valueOf(1);
+                       
+        for ( int i = 2; i <= 200; i++)
+        {
+            proizv = proizv.multiply(BigInteger.valueOf((long) i * i));
+        }
+        
+        System.out.println("Произведение квадратов первых двухсот чисел: " + proizv);
+
+    }
+    
+}
