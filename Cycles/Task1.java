4c26e3006004772757b00915cf4b02e7b6736183
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-30 11:12:21 +0200

    Update Task1.java

--- Cycles/Task1.java
+++ Cycles/Task1.java
@@ -1,1 +1,33 @@
-//
+//Циклы. 1. Напишите программу, где пользователь вводит любое целое положительное число. 
+//А программа суммирует все числа от 1 до введенного пользователем числа. 
+
+package task1;
+
+import java.util.Scanner;
+
+public class Task1 {
+      
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter an integer > 1: ");
+        int a = input.nextInt();
+        while (a <= 0)
+        {
+            System.out.print("Please enter an integer > 1: ");
+            a = input.nextInt();
+        }
+        
+        a = Math.abs(a);
+        int summa = 0;
+        
+        for (int i = 1; i <= a; i++)
+        {
+            summa += i;
+        }  
+        
+        System.out.println("The sum of the numbers is: " + summa);
+    }
+    
+}
