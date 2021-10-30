981e7b2d17c7fbd626ccb195dd7da1e974b5950b
JavaCoder2021 <92649923+JavaCoder2021@users.noreply.github.com>
2021-10-30 10:58:13 +0200

    Update Task1.java

--- Branching/Task1.java
+++ Branching/Task1.java
@@ -1,1 +1,24 @@
-//
+//Ветвления. 1. Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, 
+//и если да, то будет ли он прямоугольным. 
+
+package task1;
+
+public class Task1 {
+    
+    public static void main(String[] args) {
+        
+        double angle1 = 50.0;
+        double angle2 = 45.0;
+        
+        if ( angle1 > 0 && angle2 > 0 && ((angle1 + angle2) < 180) ) 
+            System.out.println("Triangle true");
+        else
+            System.out.println("Triangle false");
+        
+        if ((angle1 + angle2) == 90.0)
+            System.out.println("Triangle rectangular");
+        else
+            System.out.println("Triangle not rectangular");
+    }
+    
+}
