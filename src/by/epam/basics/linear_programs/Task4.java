3e372d2cfa39fa1f6b6fa2f1c235f6fe30c3cb12
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-07 20:50:03 +0200

    Create Task4.java

---
+++ src/by/epam/basics/linear_programs/Task4.java
@@ -0,0 +1,24 @@
+/*
+4. Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). 
+Поменять местами дробную и целую части числа и вывести полученное значение числа. 
+*/
+
+package by.epam.basics.linear_programs;
+
+public class Task4 {
+    
+    public static void main(String[] args) {
+        
+        double R = func(125.111);
+        System.out.printf("R = %.3f \n", R);
+       
+    }
+    
+    public static double func(double x) {
+        
+        double z = (x * 1000) % 1000 + (int) x / 1000.0;
+        return z;
+       
+    }
+    
+}
