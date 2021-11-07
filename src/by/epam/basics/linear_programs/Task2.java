2453a9321d3ff051bde4c48672a050c8feb5aff5
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-07 20:40:08 +0200

    Create Task2.java

---
+++ src/by/epam/basics/linear_programs/Task2.java
@@ -0,0 +1,27 @@
+/*
+2. Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+*/
+
+package by.epam.basics.linear_programs;
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        
+        double a = 1.0;
+        double b = 2.0;
+        double c = 3.0;
+        
+        double z = func(a, b, c);
+        System.out.println("z = " + z);
+        
+    }
+    
+    public static double func(double a, double b, double c) {  
+        
+        double z = ( b + Math.sqrt( Math.pow(b, 2) + 4 * a * c ) ) / 2 * a - Math.pow(a, 3) * c + Math.pow(b, -2);
+        return z;  
+        
+    }
+    
+}
