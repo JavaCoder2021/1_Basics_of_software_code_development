13b689842e3870a2f9813b4f8910794d96413e73
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-07 20:46:04 +0200

    Create Task3.java

---
+++ src/by/epam/basics/linear_programs/Task3.java
@@ -0,0 +1,38 @@
+/*
+3. Вычислить значение выражения по формуле (все переменные принимают действительные значения): 
+*/
+
+package by.epam.basics.linear_programs;
+
+import static java.lang.Math.*;
+
+public class Task3 {
+    
+    public static void main(String[] args) {
+        
+        double z = func(60, 40);
+        System.out.printf("z = %.1f \n", z);
+       
+    }
+    
+    /** 
+     * @param x value in degrees
+     * @param y value in degrees
+     */
+    public static double func(double x, double y) {
+        
+        double radiansX  = toRadians(x);
+        double radiansY  = toRadians(y);
+        double radiansXY = toRadians(x * y);
+        double cosX = cos(radiansX);
+        double cosY = cos(radiansY);
+        double sinX = sin(radiansX);
+        double sinY = sin(radiansY);
+        double tanXY = tan(radiansXY);
+        double z = ((sinX + cosY) / (cosX - sinY)) * tanXY;
+        
+        return z;
+       
+    }
+    
+}
