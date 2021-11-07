e8643bf99cf25679f84dedcd99457e0936deb000
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-07 20:35:45 +0200

    Update Task1.java

--- src/by/epam/basics/linear_programs/Task1.java
+++ src/by/epam/basics/linear_programs/Task1.java
@@ -1,1 +1,23 @@
-//
+/*
+1. Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c. 
+*/
+
+package by.epam.basics.linear_programs;
+
+public class Task1 {
+    
+    public static void main(String[] args) {
+        
+        double z = func(1, 2, 3);
+        System.out.printf("z = %.1f \n", z);
+        
+    }
+    
+    public static double func(double a, double b, double c) {  
+        
+        double z = (( a - 3 ) * b / 2 ) + c; 
+        return z;  
+        
+    }
+    
+}
