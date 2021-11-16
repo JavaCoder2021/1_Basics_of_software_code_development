e80f77db01aa7c317d4ccfcdebb6c28eabf36c36
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-16 19:36:42 +0200

    Create Task5.java

---
+++ src/by/epam/basics/branching/Task5.java
@@ -0,0 +1,29 @@
+/*
+Ветвления. 5. 
+Вычислить значение функции:
+*/
+
+package by.epam.basics.branching;
+
+public class Task5 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(F(2));
+            
+    }
+    
+    public static double F(double x) {
+       
+        double z;
+        
+        if ( x <=3 ) 
+            z = Math.pow(x, 2) - 3 * x + 9;
+        else 
+            z = 1 / ( Math.pow(x, 3) + 6 );
+        
+        return z;
+       
+    }    
+    
+}
