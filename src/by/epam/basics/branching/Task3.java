699bbea2d7df4db17f2b23bbfcf9f294c92b05b1
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-28 14:07:56 +0200

    Update Task3.java

--- src/by/epam/basics/branching/Task3.java
+++ src/by/epam/basics/branching/Task3.java
@@ -1,8 +1,8 @@
 /*
-Ветвления. 3. 
-Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). 
-Определить, будут ли они расположены на одной прямой. 
-*/
+ * Ветвления. 3. 
+ * Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). 
+ * Определить, будут ли они расположены на одной прямой. 
+ */
 
 package by.epam.basics.branching;
 
@@ -16,7 +16,7 @@ public class Task3 {
     
     public static void pointsOnLine(double x1, double y1, double x2, double y2, double x3, double y3) {
         
-        double k = ( (y2 - y1) / (x2 - x1) );
+        double k = ((y2 - y1) / (x2 - x1));
         double b = y1 - k * x1;
         
         if ( y3 == (k * x3 + b) ) 
