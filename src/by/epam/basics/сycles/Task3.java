bc56572dac25e3495e8f7b2bcdc5b8fb42b62fe6
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-28 14:42:36 +0200

    Create Task3.java

---
+++ src/by/epam/basics/сycles/Task3.java
@@ -0,0 +1,21 @@
+/*
+ * Циклы. 3. 
+ * Найти сумму квадратов первых ста чисел. 
+ */
+package by.epam.basics.сycles;
+
+public class Task3 {
+         
+    public static void main(String[] args) {
+        
+        int sum = 0;
+               
+        for (int i = 1; i <= 100; i++) {
+            sum += i * i;
+        }
+        
+        System.out.println("Sum of the squares of the first hundred numbers: " + sum);
+
+    }
+    
+}
