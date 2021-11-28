431aa1310380df4e9afaec932f4ce1a896b598f1
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2021-11-28 14:35:29 +0200

    Create Task2.java

---
+++ src/by/epam/basics/сycles/Task2.java
@@ -0,0 +1,29 @@
+/*
+ * Циклы. 2. 
+ * Вычислить значения функции на отрезке [а,b] c шагом h:
+ */
+package by.epam.basics.сycles;
+
+public class Task2 {
+         
+    public static void main(String[] args) {
+        
+        double a = -5;
+        double b = 10;
+        double h = 3;
+        double y;
+        
+        for ( double x = a; x <= b; x += h)
+        {
+            if ( x > 2 ) 
+                y = x;
+            else 
+                y = -x;
+            System.out.print("  " + y + "  ");
+        }
+        
+        System.out.println();
+
+    }
+    
+}
